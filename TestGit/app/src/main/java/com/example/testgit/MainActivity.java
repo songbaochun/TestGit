3288b8e55c3722c3dead0eb12728ab78c1dff1e6
unknown <>
2017-01-13 09:51:33 +0800

    --test

--- TestGit/app/src/main/java/com/example/testgit/MainActivity.java
+++ TestGit/app/src/main/java/com/example/testgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void init(){
+        Toast.makeText(this,"sdsa",Toast.LENGTH_SHORT).show();
     }
 }
